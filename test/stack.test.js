e4144f93b87d95acff28a78b952d4ead6ca8ea24
abungubrighton <brighton@ibuqacapital.com>
2023-10-22 21:54:41 +0300

    chore: first failing individual test in the stack test suite

---
+++ test/stack.test.js
@@ -0,0 +1,11 @@
+describe("My Stack", () => {
+
+    it('is crated empty', () => {
+        const stack = new Stack();
+
+        expect(stack.top).toBe(-1);
+    });
+    //  to Hold off on a test add .todo EXAMPLE: it.todo('is crated empty');
+    it('can push to the top of the stack');
+    it('can pop off the stack');
+})
