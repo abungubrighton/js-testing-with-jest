c0b02864e25f1910ba717126f7127f2bc3f5f3d9
abungubrighton <brighton@ibuqacapital.com>
2023-10-25 05:32:30 +0300

    chore:test valid state for new stack

--- test/stack.test.js
+++ test/stack.test.js
@@ -1,11 +1,23 @@
+/**GOAL: create a stack data structure and you are not allowed to use Arrays
+ * When a stack(instantiated) is created , the index of the TOP value should be -1
+ * Will use an object{} to represent the stack ( call it iteems), bcz we are not allowed to use ARRAYS
+ * Will call the object items
+ */
+class Stack {
+    constructor() {
+        this.top = -1;
+        this.items = {};
+    }
+};
 describe("My Stack", () => {
 
     it('is crated empty', () => {
         const stack = new Stack();
 
         expect(stack.top).toBe(-1);
+        expect(stack.items).toEqual({});
     });
     //  to Hold off on a test add .todo EXAMPLE: it.todo('is crated empty');
-    it('can push to the top of the stack');
-    it('can pop off the stack');
+    it.todo('can push to the top of the stack');
+    it.todo('can pop off the stack');
 })
